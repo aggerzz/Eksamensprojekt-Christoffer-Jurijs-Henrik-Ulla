5d9fcbf048574eb0135b5a1f9ef6bd42bb932446
agger <agger@DESKTOP-K1Q295O.ceuherning.int>
2017-05-05 11:01:53 +0200

    GUI

--- Eksamensprojekt/src/brugerflade/Main.java
+++ Eksamensprojekt/src/brugerflade/Main.java
@@ -1,21 +1,78 @@
 package brugerflade;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 // DOES THIS WORK?
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setTitle("Ferreri forhandler");
-			primaryStage.setScene(scene);
-			primaryStage.show();
+		        primaryStage.setTitle("Ferreri forhandler");
+		        GridPane grid = new GridPane();
+		        grid.setAlignment(Pos.CENTER);
+		        grid.setHgap(10);
+		        grid.setVgap(10);
+		        grid.setPadding(new Insets(25, 25, 25, 25));
+
+		        Text scenetitle = new Text("Ferreri forhandler");
+		        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		        grid.add(scenetitle, 0, 0, 2, 1);
+
+		        Label userName = new Label("User Name:");
+		        grid.add(userName, 0, 1);
+
+		        TextField userTextField = new TextField();
+		        grid.add(userTextField, 1, 1);
+
+		        Label pw = new Label("Password:");
+		        grid.add(pw, 0, 2);
+
+		        PasswordField pwBox = new PasswordField();
+		        grid.add(pwBox, 1, 2);
+
+		        Button btn = new Button("Sign in");
+		        HBox hbBtn = new HBox(10);
+		        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+		        hbBtn.getChildren().add(btn);
+		        grid.add(hbBtn, 1, 4);
+
+		        Text actiontarget = new Text();
+		        grid.add(actiontarget, 0, 6);
+		        GridPane.setColumnSpan(actiontarget, 2);
+		        GridPane.setHalignment(actiontarget, null);
+		        actiontarget.setId("actiontarget");
+
+		        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+		            @Override
+		            public void handle(ActionEvent e) {
+		                Menu menuStage = new Menu();
+		                menuStage.start(new Stage());
+		                primaryStage.hide();
+		            }
+		        });
+
+		        Scene scene = new Scene(grid, 300, 275);
+		        primaryStage.setScene(scene);
+		        
+		        
+		        primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
