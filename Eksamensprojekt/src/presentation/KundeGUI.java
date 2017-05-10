eab567a9aa557db3807d66b175bbf164c64846f6
Henrik Stensgaard <Henrik_stensgaard@outlook.dk>
2017-05-10 12:00:57 +0200

    tilbage knap lavet

--- Eksamensprojekt/src/presentation/KundeGUI.java
+++ Eksamensprojekt/src/presentation/KundeGUI.java
@@ -1,6 +1,5 @@
 package presentation;
 
-
 import domain.Kunde;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
@@ -21,80 +20,95 @@ import javafx.stage.Stage;
 import logic.FFLogic;
 
 public class KundeGUI extends Application {
-	public void start(Stage menuStage) {
+	public void start(Stage KundeStage) {
 		try {
-			menuStage.setTitle("Ferrari forhandler");
+			KundeStage.setTitle("Ferrari forhandler");
 			GridPane grid = new GridPane();
 			grid.setHgap(10);
 			grid.setVgap(10);
 			grid.setPadding(new Insets(25, 25, 25, 25));
-			
+
 			Text scenetitle = new Text("Opret kunde");
 			scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 			scenetitle.setFill(Color.RED);
 			grid.add(scenetitle, 0, 0, 2, 1);
-			
-			//Fornavn
+
+			// Fornavn
 			Label forNavn = new Label("Fornavn:");
 			forNavn.setTextFill(Color.RED);
 			grid.add(forNavn, 0, 1);
 			TextField forNavnTextField = new TextField();
 			grid.add(forNavnTextField, 1, 1);
-			
-			//Efternavn
+
+			// Efternavn
 			Label efterNavn = new Label("Efternavn:");
 			efterNavn.setTextFill(Color.RED);
 			grid.add(efterNavn, 0, 2);
 			TextField efterNavnTextField = new TextField();
 			grid.add(efterNavnTextField, 1, 2);
-			
-			//Adresse
+
+			// Adresse
 			Label adresse = new Label("Adresse:");
 			adresse.setTextFill(Color.RED);
 			grid.add(adresse, 0, 3);
 			TextField adresseTextField = new TextField();
 			grid.add(adresseTextField, 1, 3);
-			
-			//Postnummer
+
+			// Postnummer
 			Label postnummer = new Label("Postnummer:");
 			postnummer.setTextFill(Color.RED);
 			grid.add(postnummer, 0, 4);
 			TextField postnummerTextField = new TextField();
 			grid.add(postnummerTextField, 1, 4);
-			
-			//By
+
+			// By
 			Label byen = new Label("By:");
 			byen.setTextFill(Color.RED);
 			grid.add(byen, 0, 5);
 			TextField byenTextField = new TextField();
 			grid.add(byenTextField, 1, 5);
-			
-			//Fødselsdato
+
+			// Fødselsdato
 			Label fødselsdato = new Label("Fødselsdato:");
 			fødselsdato.setTextFill(Color.RED);
 			grid.add(fødselsdato, 0, 6);
 			TextField fødselsdatoTextField = new TextField();
 			grid.add(fødselsdatoTextField, 1, 6);
-			
-			//TelefonNummer
+
+			// TelefonNummer
 			Label telefonNummer = new Label("Telefonnummer:");
 			telefonNummer.setTextFill(Color.RED);
 			grid.add(telefonNummer, 0, 7);
 			TextField telefonNummerTextField = new TextField();
 			grid.add(telefonNummerTextField, 1, 7);
-			
-			//Email
+
+			// Email
 			Label email = new Label("Email:");
 			email.setTextFill(Color.RED);
 			grid.add(email, 0, 8);
 			TextField emailTextField = new TextField();
 			grid.add(emailTextField, 1, 8);
 			
+			Button btnTilbage = new Button("Tilbage");
+			HBox hbBtnTilbage = new HBox(7);
+			hbBtnTilbage.setAlignment(Pos.TOP_LEFT);
+			hbBtnTilbage.getChildren().add(btnTilbage);
+			grid.add(hbBtnTilbage, 13, 15);
+			btnTilbage.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent e) {
+					MenuGUI menuStage = new MenuGUI();
+					menuStage.start(new Stage());
+					KundeStage.hide();
+				}
+			});
+
+
 			Button btnOpretKunde1 = new Button("Opret kunde");
 			HBox hbBtnOpretkunde1 = new HBox(10);
 			hbBtnOpretkunde1.setAlignment(Pos.TOP_LEFT);
 			hbBtnOpretkunde1.getChildren().add(btnOpretKunde1);
-			grid.add(hbBtnOpretkunde1, 3, 3);
+			grid.add(hbBtnOpretkunde1, 15, 15);
 			btnOpretKunde1.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent e) {
@@ -120,10 +134,10 @@ public class KundeGUI extends Application {
 				}
 			});
 			Scene scene = new Scene(grid, 640, 450);
-			menuStage.setScene(scene);
+			KundeStage.setScene(scene);
 			scene.getStylesheets().addAll(this.getClass().getResource("/application/application.css").toExternalForm());
 
-			menuStage.show();
+			KundeStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -131,4 +145,3 @@ public class KundeGUI extends Application {
 	}
 
 }
-
