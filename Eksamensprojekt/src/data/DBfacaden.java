c1bdc43cf551baf4e6d388afaf795c21dc5e1496
Jurijs <Jurijs@JurijsM.ceuherning.int>
2017-05-10 09:36:04 +0200

    facaden for opret kunde opdateret

--- Eksamensprojekt/src/data/DBfacaden.java
+++ Eksamensprojekt/src/data/DBfacaden.java
@@ -7,7 +7,7 @@ public class DBfacaden {
 
 	private OpretKundeDB opretKundeInfo = new OpretKundeDB();
 
-	public void createMatchInfo(Kunde kunde) {
+	public void opretKundeInfo(Kunde kunde) {
 		opretKundeInfo.opretKunde(kunde);
 	}
 
