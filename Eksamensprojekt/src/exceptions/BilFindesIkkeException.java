dd1c02499d04df6ff7c0594800ae85dcaad4fda3
Henrik Stensgaard <Henrik_stensgaard@outlook.dk>
2017-05-15 10:48:40 +0200

    tilføjet nye exceptions

---
+++ Eksamensprojekt/src/exceptions/BilFindesIkkeException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class BilFindesIkkeException extends Exception {
+
+}
---
+++ Eksamensprojekt/src/exceptions/SælgerFindesIkkeException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class SælgerFindesIkkeException extends Exception {
+
+}
