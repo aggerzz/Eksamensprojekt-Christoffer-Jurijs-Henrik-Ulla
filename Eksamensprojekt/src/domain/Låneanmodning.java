a2e7040343d4d7738fcbcaedf9af8ec1111f0969
Henrik Stensgaard <Henrik_stensgaard@outlook.dk>
2017-05-16 09:45:19 +0200

    tilføjet get og set metoder

--- Eksamensprojekt/src/domain/Låneanmodning.java
+++ Eksamensprojekt/src/domain/Låneanmodning.java
@@ -1,5 +1,23 @@
 package domain;
 
-public interface Låneanmodning {
+import exceptions.TelefonnummerIkkeOplystException;
+import exceptions.PersonnummerIkkeUdfyldtException;
+import exceptions.KreditværdighedIkkeUdfyldtException;
+import exceptions.RentesatsIkkeUdfyldtException;
+import exceptions.StelnummerIkkeOplystException;
+import exceptions.LøbetidIkkeUdfyldtException;
 
+public interface Låneanmodning {
+	public String getPersonNummer() throws PersonnummerIkkeUdfyldtException;
+	public void setPersonNummer(String personNummer);
+	public String getTelefonNummer() throws TelefonnummerIkkeOplystException;
+	public void setTelefonNummer(String telefonNummer);
+	public String getKreditværdihed() throws KreditværdighedIkkeUdfyldtException;
+	public void setKreditværdighed(String kreditværdighed);
+	public String getRentesats() throws RentesatsIkkeUdfyldtException;
+	public void setRentesats(String rentesats);
+	public String getStelNummer() throws StelnummerIkkeOplystException;
+	public void setStelNummer(String stelNummer);
+	public String getLøbetid() throws LøbetidIkkeUdfyldtException;
+	public void setLøbetid(String løbetid);
 }
--- Eksamensprojekt/src/domain/Låneanmodninglmpl.java
+++ Eksamensprojekt/src/domain/Låneanmodninglmpl.java
@@ -1,5 +1,84 @@
 package domain;
 
+import exceptions.KreditværdighedIkkeUdfyldtException;
+import exceptions.LøbetidIkkeUdfyldtException;
+import exceptions.PersonnummerIkkeUdfyldtException;
+import exceptions.RentesatsIkkeUdfyldtException;
+import exceptions.StelnummerIkkeOplystException;
+import exceptions.TelefonnummerIkkeOplystException;
+
 public class Låneanmodninglmpl implements Låneanmodning {
 
+	@Override
+	public String getPersonNummer() throws PersonnummerIkkeUdfyldtException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setPersonNummer(String personNummer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getTelefonNummer() throws TelefonnummerIkkeOplystException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setTelefonNummer(String telefonNummer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getKreditværdihed() throws KreditværdighedIkkeUdfyldtException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setKreditværdighed(String kreditværdighed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getRentesats() throws RentesatsIkkeUdfyldtException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setRentesats(String rentesats) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getStelNummer() throws StelnummerIkkeOplystException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setStelNummer(String stelNummer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getLøbetid() throws LøbetidIkkeUdfyldtException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLøbetid(String løbetid) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
