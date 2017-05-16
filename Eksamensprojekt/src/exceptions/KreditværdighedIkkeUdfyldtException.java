1e865e35a46d53b23f0a92e9caf5f6fd6295dede
Henrik Stensgaard <Henrik_stensgaard@outlook.dk>
2017-05-16 09:33:06 +0200

    tilføjet nye exceptions

---
+++ Eksamensprojekt/src/exceptions/KreditværdighedIkkeUdfyldtException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class KreditværdighedIkkeUdfyldtException extends Exception {
+
+}
---
+++ Eksamensprojekt/src/exceptions/PersonnummerIkkeUdfyldtException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class PersonnummerIkkeUdfyldtException extends Exception {
+
+}
