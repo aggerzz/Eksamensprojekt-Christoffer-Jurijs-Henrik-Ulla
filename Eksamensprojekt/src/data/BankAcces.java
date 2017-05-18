2348110728ae4ce905914695620fe7c8c5af9cb7
agger <agger@DESKTOP-K1Q295O.ceuherning.int>
2017-05-18 09:22:12 +0200

    Mabye bankrente

---
+++ Eksamensprojekt/src/data/BankAcces.java
@@ -0,0 +1,25 @@
+package data;
+import logic.BeregnRente;
+import com.ferrari.finances.dk.bank.InterestRate;
+
+public class BankAcces implements Runnable{
+	
+private BeregnRente beregnRente;
+
+public BankAcces( BeregnRente beregnRente) {
+	this.beregnRente = beregnRente;
+}
+
+@Override
+public void run() {
+
+	beregnRente.todaysRate = getTodaysRate();
+
+}
+
+public double getTodaysRate() {
+	InterestRate interestRate = InterestRate.i();
+
+	return interestRate.todaysRate();
+}
+}
